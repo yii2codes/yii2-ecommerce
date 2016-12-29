dd669ff8480952bebe4f00693f483d514fa16fcb
Anil Chaudhari <imanilchaudhari@gmail.com>
2016-12-29 13:53:42 +0545

    initial commit

---
+++ backend/controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+namespace backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+/**
+ * Test controller
+ */
+class TestController extends Controller
+{
+    public function actionTest(){
+
+
+    }
+}
